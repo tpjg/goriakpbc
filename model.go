e6d6ad488ef837ae90efd00482753185448ecec1
Timo Gatsonides <tpjg@me.com>
2014-03-11 15:24:33 +0100

    Change so only Indexes holds the indexes instead of having both Indexes and MultiIndexes
    This commit is to implement the Indexes having multiple values in the most direct way. This may break existing applications, but will be obvious and the compiler will pinpoint the necessary changes.
    
    See Github issue 71, "Since the Indexes are directly accessible (not through getter/setter functions) you can manipulate the values and as @MJDSys wrote changing values will result in both old and new values being persisted. This will break older applications in a subtle and obscure way - e.g. when searching for the old value will return items no longer expected."

--- model.go
+++ model.go
@@ -567,21 +567,11 @@ func (m *Model) Vclock() (vclock []byte) {
 
 // Return the object's indexes.  This allows an application to set custom secondary
 // indexes on the object for later querying.
-func (m *Model) Indexes() map[string]string {
+func (m *Model) Indexes() map[string][]string {
 	if m.robject.Indexes == nil {
-		m.robject.Indexes = make(map[string]string)
+		m.robject.Indexes = make(map[string][]string)
 	}
 	return m.robject.Indexes
-}
-
-// Return the object's indexes.  This allows an application to set custom secondary
-// indexes on the object for later querying.
-// This allows to put several values to the same tag.
-func (m *Model) MultiIndexes() map[string][]string {
-	if m.robject.MultiIndexes == nil {
-		m.robject.MultiIndexes = make(map[string][]string)
-	}
-	return m.robject.MultiIndexes
 }
 
 // Get a models Key, e.g. needed when Riak has picked it
