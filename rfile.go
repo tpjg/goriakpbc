1c7313cb22571ab2a338f7fc7c83c4935fcfb93d
Timo Gatsonides <tpjg@me.com>
2014-03-30 22:19:58 +0200

    Adding "RFile" or bigger objects support
    Implements the Reader, Writer and Seeker interfaces (see io package)

---
+++ rfile.go
@@ -0,0 +1,236 @@
+package riak
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"strconv"
+)
+
+/* The RFile struct stores (large) values in Riak and behaves very similar
+to a regular os.File object. It implements the io.Reader, io.Writer and
+io.Seeker interfaces. The value is split into chunks because really large
+values (>10Mb) can't be stored efficiently in Riak and also because growing
+or changing large values is inefficient (changing a single byte would require
+a PUT of the entire, possibly large, value).
+*/
+type RFile struct {
+	client     *Client
+	root       *RObject // The "root", holding meta-data only
+	chunk      *RObject // The current chunk or segment of data
+	chunk_size int
+	pos        int
+	size       int
+}
+
+var (
+	NotFile     = errors.New("Not suitable to use as RFile")
+	ErrorInFile = errors.New("Error in RFile")
+)
+
+// Return the Key for a specific chunk
+func chunkKey(key string, chunkno int) string {
+	return fmt.Sprintf("%v-%06d", key, chunkno)
+}
+
+// Create a new RFile. Will overwrite/truncate existing data.
+func (c *Client) CreateFile(bucketname string, key string, contentType string, chunk_size int, options ...map[string]uint32) (*RFile, error) {
+	bucket, err := c.Bucket(bucketname)
+	if err != nil {
+		return nil, err
+	}
+	// Create the root object, holding meta-data only
+	root := bucket.NewObject(key, options...)
+	root.ContentType = contentType
+	root.Data = []byte{}
+	root.Meta["chunk_size"] = strconv.Itoa(chunk_size)
+	root.Meta["chunk_count"] = strconv.Itoa(0)
+	err = root.Store()
+	if err != nil {
+		return nil, err
+	}
+	// Return the completed struct
+	return &RFile{c, root, nil, chunk_size, 0, 0}, nil
+}
+
+// Open a File. Will return an error if it does not exist in Riak yet or does
+// not have the correct meta-tags to support File-like operations.
+func (c *Client) OpenFile(bucketname string, key string, contentType string, options ...map[string]uint32) (*RFile, error) {
+	root, err := c.GetFrom(bucketname, key, options...)
+	if err != nil {
+		return nil, err
+	}
+	chunk_size, err := strconv.Atoi(root.Meta["chunk_size"])
+	if err != nil || chunk_size < 1 || chunk_size > 100*1024*1024 {
+		// Supports chunks up to 100Mb, there is some conflicting information about maximum
+		// value size in Riak ranging from 100Kb (for low latency guarantees) to 20Mb.
+		return nil, NotFile
+	}
+	chunk_count, err := strconv.Atoi(root.Meta["chunk_count"])
+	if err != nil || chunk_count < 0 {
+		return nil, NotFile
+	}
+	// Determine the size by looking at the last chunk
+	if chunk_count > 0 {
+		chunk, err := c.GetFrom(bucketname, chunkKey(key, chunk_count-1))
+		if err != nil {
+			return nil, ErrorInFile
+		}
+		return &RFile{c, root, chunk, chunk_size, 0, (chunk_count-1)*chunk_size + len(chunk.Data)}, nil
+	}
+	// Otherwise size is 0
+	return &RFile{c, root, nil, chunk_size, 0, 0}, nil
+}
+
+// Implements the io.Seeker interface
+func (r *RFile) Seek(offset int64, whence int) (int64, error) {
+	if whence == 0 {
+		if offset < 0 || int(offset) > r.size {
+			return int64(r.pos), io.EOF
+		}
+		r.pos = int(offset)
+	} else if whence == 1 {
+		if r.pos+int(offset) < 0 || r.pos+int(offset) > r.size {
+			return int64(r.pos), io.EOF
+		}
+		r.pos += int(offset)
+	} else if whence == 2 {
+		if r.size+int(offset) < 0 || r.size+int(offset) > r.size {
+			return int64(r.pos), io.EOF
+		}
+		r.pos = r.size + int(offset)
+	}
+	return int64(r.pos), nil
+}
+
+// Implements the io.Writer interface
+func (r *RFile) Write(p []byte) (n int, err error) {
+	fmt.Printf("Write(%v bytes) - ", len(p))
+	wpos := 0 // Keep track how much of p has been written
+	for wpos < len(p) {
+		k := chunkKey(r.root.Key, r.pos/r.chunk_size)
+		// Check if a chunk must be loaded (position<size or not completely written)
+		if r.pos < r.size || r.size%r.chunk_size != 0 {
+			// If the current chunk happens to be the one we're looking for, then skip loading.
+			if !(r.chunk != nil && r.chunk.Key == k) {
+				fmt.Printf("Getting chunk %v - ", k)
+				r.chunk, err = r.client.GetFrom(r.root.Bucket.name, k, r.root.Options...)
+				if err != nil {
+					return wpos, err
+				}
+			}
+		} else {
+			// Create a new chunk
+			fmt.Printf("Creating chunk %v - ", k)
+			r.chunk, err = r.client.NewObjectIn(r.root.Bucket.name, k, r.root.Options...)
+			r.chunk.ContentType = r.root.ContentType
+			if err != nil {
+				return wpos, err
+			}
+		}
+		// Check where to start writing within the chunk
+		cpos := r.pos % r.chunk_size
+		// Determine how many bytes to write into this chunk (fill up to chunk_size)
+		towrite := len(p) - wpos
+		if towrite > (r.chunk_size - cpos) {
+			towrite = r.chunk_size - cpos
+		}
+		fmt.Printf("cpos=%v , towrite=%v , len(chunk)=%v\n", cpos, towrite, len(r.chunk.Data))
+		// Check if the chunk Data is large enough, otherwise write the first part
+		// and then append the last part.
+		check := 0
+		if len(r.chunk.Data) == 0 {
+			// Specialized case for a new chunk
+			fmt.Printf("Creating new data for chunk of %v\n", towrite)
+			r.chunk.Data = make([]byte, towrite)
+			check = copy(r.chunk.Data, p[wpos:wpos+towrite])
+		} else if len(r.chunk.Data) < cpos+towrite {
+			if cpos == len(r.chunk.Data) {
+				// Just append to the chunk
+				fmt.Printf("Appending data to chunk\n")
+				r.chunk.Data = append(r.chunk.Data, p[wpos:wpos+towrite]...)
+				check = towrite // just assume
+			} else {
+				// Copy the part that fits, then append
+				part := len(r.chunk.Data) - cpos
+				fmt.Printf("Copy part %v and then appending %v\n", part, towrite-part)
+				check = copy(r.chunk.Data[cpos:], p[wpos:wpos+part])
+				r.chunk.Data = append(r.chunk.Data, p[wpos+part:wpos+towrite-part]...)
+				check += towrite - part
+			}
+		} else {
+			// Copy to the chunk
+			fmt.Printf("Copying to chunk\n")
+			check = copy(r.chunk.Data[cpos:], p[wpos:wpos+towrite])
+		}
+		if check != towrite {
+			errors.New("Should never happen!")
+		}
+		// Save the chunk to Riak
+		err = r.chunk.Store()
+		if err != nil {
+			return wpos, err
+		}
+		// Update the counters
+		r.pos += towrite
+		wpos += towrite
+		// Update the size if necessary
+		if r.pos > r.size {
+			if (r.pos / r.chunk_size) > (r.size / r.chunk_size) {
+				// Update the root KV
+				r.root.Meta["chunk_count"] = strconv.Itoa(r.pos/r.chunk_size + 1)
+				fmt.Printf("Updating chunk_count to %v\n", r.root.Meta["chunk_count"])
+				err = r.root.Store()
+				if err != nil {
+					return wpos, nil
+				}
+			}
+			r.size = r.pos
+		}
+	}
+	// Return the number of bytes written
+	return wpos, nil
+}
+
+// Implements the io.Reader interface
+func (r *RFile) Read(p []byte) (n int, err error) {
+	fmt.Printf("Read(%v bytes) - ", len(p))
+	rpos := 0 // Keep track how much of p has been read
+	for rpos < len(p) {
+		k := chunkKey(r.root.Key, r.pos/r.chunk_size)
+		// Check if a chunk must be loaded
+		if r.pos < r.size || r.size%r.chunk_size != 0 {
+			// If the current chunk happens to be the one we're looking for, then skip loading.
+			if !(r.chunk != nil && r.chunk.Key == k) {
+				fmt.Printf("Getting chunk %v - ", k)
+				r.chunk, err = r.client.GetFrom(r.root.Bucket.name, k, r.root.Options...)
+				if err != nil {
+					return rpos, err
+				}
+			}
+		} else {
+			// Reading beyong EOF
+			return rpos, io.EOF
+		}
+		// Check where to start reading within the chunk
+		cpos := r.pos % r.chunk_size
+		// Determine how many bytes to write into this chunk (fill up to chunk_size)
+		toread := len(p) - rpos
+		if toread > (r.chunk_size - cpos) {
+			toread = r.chunk_size - cpos
+		}
+		fmt.Printf("cpos=%v , toread=%v , len(chunk)=%v\n", cpos, toread, len(r.chunk.Data))
+		// Check if the chunk Data is large enough, otherwise read it and return EOF
+		if len(r.chunk.Data[cpos:]) < toread {
+			copy(p, r.chunk.Data[cpos:])
+			return rpos + len(r.chunk.Data[cpos:]), io.EOF
+		}
+		// Read the chunk
+		copy(p, r.chunk.Data[cpos:])
+		// Update counters
+		r.pos += len(r.chunk.Data[cpos:])
+		rpos += len(r.chunk.Data[cpos:])
+	}
+	// Return the number of bytes written
+	return rpos, nil
+}
---
+++ rfile_test.go
@@ -0,0 +1,101 @@
+package riak
+
+import (
+	"fmt"
+	"github.com/bmizerany/assert"
+	"io"
+	"testing"
+)
+
+func TestRFile(t *testing.T) {
+	fmt.Printf("Testing experimental RFile stuff\n")
+	client := setupConnection(t)
+	assert.T(t, client != nil)
+	f, err := client.CreateFile("rfile_test.go", "test", "text/plain", 1024)
+	fmt.Printf("CreateFile - %v %v\n", f, err)
+	assert.T(t, err == nil)
+	assert.T(t, f != nil)
+	b, err := f.Write([]byte{'1', '2', '3', '4', '5', '6', '7', '8'})
+	assert.T(t, err == nil)
+	assert.T(t, b == 8)
+	// Add a full chunk
+	extra := make([]byte, 1024)
+	b, err = f.Write(extra)
+	assert.T(t, err == nil)
+	assert.T(t, b == 1024)
+
+	// Now rewind and check some content
+	f.Seek(0, 0)
+	b, err = f.Write([]byte{'a', 'b', 'c'}) // Overwriting first three bytes
+	assert.T(t, err == nil)
+	assert.T(t, b == 3)
+	f.Seek(1024, 0)
+	b, err = f.Write([]byte{'d', 'e', 'f'}) // Overwriting first three bytes in 2nd chunk
+	assert.T(t, err == nil)
+	assert.T(t, b == 3)
+	// Seek to the end
+	bp, err := f.Seek(0, 2)
+	assert.T(t, err == nil)
+	assert.T(t, bp == 1032)
+	b, err = f.Write([]byte{'z'}) // Append one byte
+	assert.T(t, err == nil)
+	assert.T(t, b == 1)
+
+	// Test the Reader functionality
+	buf := make([]byte, 1024)
+	f.Seek(0, 0)
+	// Read the first chunk
+	b, err = f.Read(buf)
+	assert.T(t, err == nil)
+	assert.T(t, b == 1024)
+	assert.T(t, buf[0] == 'a')
+	assert.T(t, buf[1] == 'b')
+	assert.T(t, buf[2] == 'c')
+	assert.T(t, buf[3] == '4')
+	assert.T(t, buf[7] == '8')
+	assert.T(t, buf[8] == 0)
+	// Read the second chunk
+	b, err = f.Read(buf)
+	assert.T(t, err == io.EOF)
+	assert.T(t, b == 9)
+	assert.T(t, buf[0] == 'd')
+	assert.T(t, buf[1] == 'e')
+	assert.T(t, buf[2] == 'f')
+	assert.T(t, buf[3] == 0)
+	assert.T(t, buf[8] == 'z')
+
+	// Now there should be three Riak objects: test, test-000000 and test-000001 in the rfile_test.go bucket
+	// Check <test> and cleanup
+	obj, err := GetFrom("rfile_test.go", "test")
+	assert.T(t, err == nil)
+	assert.T(t, obj.ContentType == "text/plain")
+	assert.T(t, obj.Meta["chunk_size"] == "1024")
+	assert.T(t, obj.Meta["chunk_count"] == "2")
+	obj.Destroy()
+	// Check <test-0000000> and cleanup
+	obj, err = GetFrom("rfile_test.go", "test-000000")
+	assert.T(t, err == nil)
+	assert.T(t, obj.ContentType == "text/plain")
+	assert.T(t, len(obj.Data) == 1024)
+	assert.T(t, obj.Data[0] == 'a')
+	assert.T(t, obj.Data[1] == 'b')
+	assert.T(t, obj.Data[2] == 'c')
+	assert.T(t, obj.Data[3] == '4')
+	assert.T(t, obj.Data[7] == '8')
+	assert.T(t, obj.Data[8] == 0)
+	obj.Destroy()
+	// Check <test-0000001> and cleanup
+	obj, err = GetFrom("rfile_test.go", "test-000001")
+	assert.T(t, err == nil)
+	assert.T(t, obj.ContentType == "text/plain")
+	assert.T(t, len(obj.Data) == 9)
+	assert.T(t, obj.Data[0] == 'd')
+	assert.T(t, obj.Data[1] == 'e')
+	assert.T(t, obj.Data[2] == 'f')
+	assert.T(t, obj.Data[3] == 0)
+	assert.T(t, obj.Data[8] == 'z')
+	obj.Destroy()
+
+	// Cleanup
+	fmt.Printf("End of : Testing experimental RFile stuff\n")
+}
